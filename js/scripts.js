26bf730194d7f418378dac0b30e7a937dfa23922
eric <eric.kingoo2014@gmail.com>
2019-08-23 14:08:33 +0300

    added a hover function of class cards in portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,6 @@ $('.main').click(function () {
   $(this).children('.hide').slideToggle(1200);
   $(this).children('.show').toggle(1500);
 });
+$(".card").hover(function () {
+  $(this).children(".portfolio").fadeToggle(1000, "linear");
+});
