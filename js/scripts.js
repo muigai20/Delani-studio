eec266120cf291e11a07f5d17f3d913709b92bd8
eric <eric.kingoo2014@gmail.com>
2019-08-25 00:21:43 +0300

    i have added a feed back to he user using js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,8 @@ $('.main').click(function () {
 $(".card").hover(function () {
   $(this).children(".portfolio").fadeToggle(1200, "linear");
 });
+$("button").click(function (e) {
+  var client = document.getElementById('name').value;
+  alert(' Dear ' + ' esteemed ' + client + ' we have received your message. ' + ' Thank you for the feedback.');
+  e.preventDefault();
+});
